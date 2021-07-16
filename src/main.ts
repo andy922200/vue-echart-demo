21917d9ef1e8f2a70fadbd670dc759df95c03530
Andy Lien <andy922200@gmail.com>
2021-07-16 13:11:15 +0800

    1. update settings and add demo

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,27 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { use } from 'echarts/core'
+import { CanvasRenderer } from 'echarts/renderers'
+import { PieChart } from 'echarts/charts'
+import {
+  TitleComponent,
+  TooltipComponent,
+  LegendComponent
+} from 'echarts/components'
+import ECharts from 'vue-echarts'
 
 Vue.config.productionTip = false
+
+use([
+  CanvasRenderer,
+  PieChart,
+  TitleComponent,
+  TooltipComponent,
+  LegendComponent
+])
+
+Vue.component('v-chart', ECharts)
 
 new Vue({
   router,
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,80 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div class="wrapper">
+      <div class="pie-chart">
+        <v-chart :option="pieOption" style="height:600px;width:600px;"/>
+      </div>
+      <div class="pie-chart">
+        <v-chart style="height:600px;width:600px;"/>
+      </div>
+    </div>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'About',
+  data () {
+    return {
+      pieOption: {
+        title: {
+          text: 'Traffic Sources',
+          left: 'center'
+        },
+        tooltip: {
+          trigger: 'item',
+          formatter: '{a} <br/>{b} : {c} ({d}%)'
+        },
+        legend: {
+          orient: 'vertical',
+          left: 'left',
+          data: [
+            'Direct',
+            'Email',
+            'Ad Networks',
+            'Video Ads',
+            'Search Engines'
+          ]
+        },
+        series: [
+          {
+            name: 'Traffic Sources',
+            type: 'pie',
+            radius: '55%',
+            center: ['50%', '60%'],
+            data: [
+              { value: 335, name: 'Direct' },
+              { value: 310, name: 'Email' },
+              { value: 234, name: 'Ad Networks' },
+              { value: 135, name: 'Video Ads' },
+              { value: 1548, name: 'Search Engines' }
+            ],
+            emphasis: {
+              itemStyle: {
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: 'rgba(0, 0, 0, 0.5)'
+              }
+            }
+          }
+        ]
+      }
+
+    }
+  }
+})
+</script>
+
+<style lang="scss">
+.wrapper{
+  display: flex;
+  justify-content: center;
+
+  .pie-chart{
+    width:50%;
+  }
+}
+</style>
